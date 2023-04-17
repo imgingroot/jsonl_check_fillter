a21ddf708b3548cdbdae28ae1b7ddcfa665f4ec0
imgingroot <125806651+imgingroot@users.noreply.github.com>
2023-04-17 05:07:08 +0000

    基础功能

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # jsonl_check_fillter
+
+检查jsonl文件是否可被解析，并把可解析的行和解析出错的行分别输出到两个文件
+
+输入：需要解析的jsonl文件目录（会递归遍历）
+输出：在jsonl文件同目录输出 .jsonl.checked (验证通过的行) 和 .jsonl.err (验证失败的行)
+
+执行日志在 json_check.log
+
+需要将jsonl.checked覆盖回jsonl可以使用下面的linux命令
+```
+    find /path/to/a -type f -name "*.jsonl.checked" -exec sh -c 'mv "$0" "${0%.*}"' {} \;
+```
+
+需要把所有的err文件移动，可以使用：
+```
+    find /path/to/a -type f -name "*.json.err" -exec mv {} ./ \;
+```
---
+++ json_check.py
@@ -0,0 +1,46 @@
+import json
+import os
+import logging
+import argparse
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(iterator, *args, **kwargs):
+        return iterator
+
+logging.basicConfig(filename='json_check.log', level=logging.INFO)
+
+
+def recursive_parse_jsonl_files(input_path):
+    # 遍历目录及子目录查找 jsonl 文件
+    for root, dirs, files in os.walk(input_path):
+        for file_name in files:
+            if file_name.endswith('.jsonl'):
+                input_file = os.path.join(root, file_name)
+                checked_file = os.path.join(root, f"{file_name}.checked")
+                err_file = os.path.join(root, f"{file_name}.err")
+                try:
+                    logging.info(f"Parsing jsonl files: {input_file}")
+                    parse_jsonl_file(input_file, checked_file, err_file)
+                except Exception as e:
+                    logging.exception(f"Error occurred while processing the file: {input_file}. Exception: {e}")
+
+
+def parse_jsonl_file(input_file, checked_file, err_file):
+    with open(input_file, 'r') as f, open(checked_file, 'w') as c, open(err_file, 'w') as ef:
+        for line in tqdm(f):
+            try:
+                json.loads(line)  # 解析 JSON 数据
+                c.write(line)  # 写入 checked 文件
+            except Exception as e:
+                logging.error(f"Error occurred while parsing {input_file} at line: {line.strip()}. Exception: {e}")
+                ef.write(line)  # 写入 err 文件
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parse jsonl files')
+    parser.add_argument('input_path', type=str, help='input path to recurse')
+    args = parser.parse_args()
+
+    recursive_parse_jsonl_files(args.input_path)
+    logging.info("Finished parsing all jsonl files in the input path")
